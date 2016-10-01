8d9315cdc9ab2eac523df68b5a96746e2bb48b2c
Alex Manly <alex.manly@sendachi.com>
2016-10-01 17:10:13 +0100

    Added readme for vault and consul

---
+++ terraform-application/README.md
@@ -0,0 +1,3 @@
+# Deploy HashiCorp Demo Java Apps to AWS, using Consul, Terraform and Vault
+
+This folder contains modules for Terraform that can setup the Java applications for the HashiCorp demo applications. The infrastructure provider that is used is designated.  See the `variables.tf` file in each for more documentation. 
--- terraform-vault/build.sh
+++ terraform-vault/build.sh
@@ -2,12 +2,13 @@
 set -e
 
 echo "Started: $(date)"
+echo "Please, enter the consul address (URL:PORT)"
+read CONSUL_ADDR
 export AAKI=$(sed "2q;d" ~/.aws/credentials | awk -F'=' '{print $2}' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 export ASAK=$(sed "3q;d" ~/.aws/credentials | awk -F'=' '{print $2}' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-export CONSUL=ec2-52-90-110-123.compute-1.amazonaws.com:8500
-TF_VAR_access_key=${AAKI} TF_VAR_secret_key=${ASAK} TF_VAR_consul-url=${CONSUL} terraform destroy -force
+TF_VAR_access_key=${AAKI} TF_VAR_secret_key=${ASAK} TF_VAR_consul-url=$CONSUL_ADDR terraform destroy -force
 if [ "$#" -ne 1 ]; then
-  TF_VAR_access_key=${AAKI} TF_VAR_secret_key=${ASAK} TF_VAR_consul-url=${CONSUL} terraform apply
+  TF_VAR_access_key=${AAKI} TF_VAR_secret_key=${ASAK} TF_VAR_consul-url=$CONSUL_ADDR terraform apply
 fi
 
 echo "Finished: $(date)"
