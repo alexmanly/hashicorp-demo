9a6d7edf68f6bf9604711b5d5e87e514ca647be6
Alex Manly <alex.manly@sendachi.com>
2016-10-07 11:33:45 +0100

    Added ports 8080 and 8090 in ingress

--- terraform-hashi-clients/main.tf
+++ terraform-hashi-clients/main.tf
@@ -167,6 +167,20 @@ resource "aws_security_group" "hashi_clients" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 8090
+        to_port = 8090
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     // These are for maintenance
     ingress {
         from_port = 22
